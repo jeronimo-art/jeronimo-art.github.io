08adfd33b5271bbcb037d548b239ea56b0ffc634
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-07 21:34:21 -0500

    r345

--- js/main.js
+++ js/main.js
@@ -1,32 +1,17 @@
-let ubicacionPrincipal = window.pageYOffset; //0
+const hamburguer = document.querySelector(".hamburguer");
+const menu = document.querySelector(".menu-navegacion");
 
-  AOS.init();
+hamburguer.addEventListener("click", () => {
+  menu.classList.toggle("spread");
+});
 
-window.addEventListener("scroll", function(){
-    let desplazamientoActual = window.pageYOffset; //180
-    if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
-        document.getElementsByTagName("nav")[0].style.top = "0px"
-    }else{
-        document.getElementsByTagName("nav")[0].style.top = "-100px"
-    }
-    ubicacionPrincipal= desplazamientoActual; //200
-
-})
-
-// Menu
-
-let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
-let semaforo = true;
-
-document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
-    if(semaforo){
-        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
-        semaforo= false;
-    }else{
-        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
-        semaforo= true;
-    }
-
-    enlacesHeader.classList.toggle("menudos")
-})
-
+window.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("spread") &&
+    e.target != menu &&
+    e.target != hamburguer
+  ) {
+    console.log("cerrar");
+    menu.classList.toggle("spread");
+  }
+});
