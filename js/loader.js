61a38e18e919de4a768b59669ad2601b293c94bd
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-27 15:32:18 -0500

    13123

--- js/loader.js
+++
@@ -1,5 +0,0 @@
-/*window.onload = function(){
-    var  contenedor = document.getElementById('contenedor-carga');
-    contenedor.style.visibility = 'hidden';
-    contenedor.style.opacity = '0';
-}*/
