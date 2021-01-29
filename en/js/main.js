177a93ade8fbee4470d7de2888dbb2c90a3da8e1
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-29 17:00:49 -0500

    76

--- en/js/main.js
+++ en/js/main.js
@@ -30,3 +30,24 @@ window.addEventListener("click", function (e) {
     }, 900);
   }
 });
+
+
+
+
+
+
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer){
+        console.log('cerrar')
+        menu.classList.toggle("spread")
+    }
+})
+
