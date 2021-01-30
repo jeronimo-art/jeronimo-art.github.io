a08451791ce5d981c6f91e2c2f7dcdce873185fd
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-30 18:58:58 -0500

    4234

--- en/briefcase/js/main.js
+++ en/briefcase/js/main.js
@@ -1,10 +1,6 @@
+//=============== modal #1 ===============
 let cerrar = document.querySelectorAll(".close")[0];
 let abrir = document.querySelectorAll(".cta")[0];
-let abrir2 = document.querySelectorAll(".cta-2")[0];
-let abrir3 = document.querySelectorAll(".cta-3")[0];
-let abrir4 = document.querySelectorAll(".cta-4")[0];
-let abrir5 = document.querySelectorAll(".cta-5")[0];
-let abrir6 = document.querySelectorAll(".cta-6")[0];
 let modal = document.querySelectorAll(".modal")[0];
 let modalC = document.querySelectorAll(".modal-container")[0];
 
@@ -14,42 +10,6 @@ abrir.addEventListener("click", function (e) {
   modalC.style.visibility = "visible";
   modal.classList.toggle("modal-close");
 });
-
-abrir2.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
-});
-
-abrir3.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
-});
-
-abrir4.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
-});
-
-abrir5.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
-});
-
-abrir6.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
-});
-
 
 cerrar.addEventListener("click", function () {
   modal.classList.toggle("modal-close");
@@ -72,18 +32,191 @@ window.addEventListener("click", function (e) {
   }
 });
 
+//=============== modal #2 ===============
+let abrir2 = document.querySelectorAll(".cta-2")[0];
+let modal2 = document.querySelectorAll(".modal-2")[0];
+let modalC2 = document.querySelectorAll(".modal-container-2")[0];
 
-const hamburguer = document.querySelector('.hamburguer')
-const menu = document.querySelector('.menu-navegacion')
+abrir2.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalC2.style.opacity = "1";
+  modalC2.style.visibility = "visible";
+  modal2.classList.toggle("modal-close");
+});
 
-hamburguer.addEventListener('click', ()=>{
-    menu.classList.toggle("spread")
-})
+cerrar.addEventListener("click", function () {
+  modal2.classList.toggle("modal-close");
 
-window.addEventListener('click', e =>{
-    if(menu.classList.contains('spread') 
-        && e.target != menu && e.target != hamburguer){
-        console.log('cerrar')
-        menu.classList.toggle("spread")
-    }
-})
+  setTimeout(function () {
+    modalC2.style.opacity = "0";
+    modalC2.style.visibility = "hidden";
+  }, 600);
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target == modalC2) {
+    modal2.classList.toggle("modal-close");
+
+    setTimeout(function () {
+      modalC2.style.opacity = "0";
+      modalC2.style.visibility = "hidden";
+    }, 900);
+  }
+});
+
+//=============== modal #3 ===============
+
+let abrir3 = document.querySelectorAll(".cta-3")[0];
+let modal3 = document.querySelectorAll(".modal-3")[0];
+let modalC3 = document.querySelectorAll(".modal-container-3")[0];
+
+abrir3.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalC3.style.opacity = "1";
+  modalC3.style.visibility = "visible";
+  modal3.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function () {
+  modal3.classList.toggle("modal-close");
+
+  setTimeout(function () {
+    modalC3.style.opacity = "0";
+    modalC3.style.visibility = "hidden";
+  }, 600);
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target == modalC3) {
+    modal3.classList.toggle("modal-close");
+
+    setTimeout(function () {
+      modalC3.style.opacity = "0";
+      modalC3.style.visibility = "hidden";
+    }, 900);
+  }
+});
+
+//=============== modal #4 ===============
+
+let abrir4 = document.querySelectorAll(".cta-4")[0];
+let modal4 = document.querySelectorAll(".modal-4")[0];
+let modalC4 = document.querySelectorAll(".modal-container-4")[0];
+
+abrir4.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalC4.style.opacity = "1";
+  modalC4.style.visibility = "visible";
+  modal4.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function () {
+  modal4.classList.toggle("modal-close");
+
+  setTimeout(function () {
+    modalC4.style.opacity = "0";
+    modalC4.style.visibility = "hidden";
+  }, 600);
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target == modalC4) {
+    modal4.classList.toggle("modal-close");
+
+    setTimeout(function () {
+      modalC4.style.opacity = "0";
+      modalC4.style.visibility = "hidden";
+    }, 900);
+  }
+});
+
+//=============== modal #5 ===============
+
+let abrir5 = document.querySelectorAll(".cta-5")[0];
+let modal5 = document.querySelectorAll(".modal-5")[0];
+let modalC5 = document.querySelectorAll(".modal-container-5")[0];
+
+abrir5.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalC5.style.opacity = "1";
+  modalC5.style.visibility = "visible";
+  modal5.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function () {
+  modal5.classList.toggle("modal-close");
+
+  setTimeout(function () {
+    modalC5.style.opacity = "0";
+    modalC5.style.visibility = "hidden";
+  }, 600);
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target == modalC5) {
+    modal5.classList.toggle("modal-close");
+
+    setTimeout(function () {
+      modalC5.style.opacity = "0";
+      modalC5.style.visibility = "hidden";
+    }, 900);
+  }
+});
+
+//=============== modal #6 ===============
+
+let abrir6 = document.querySelectorAll(".cta-6")[0];
+let modal6 = document.querySelectorAll(".modal-6")[0];
+let modalC6 = document.querySelectorAll(".modal-container-6")[0];
+
+abrir6.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalC6.style.opacity = "1";
+  modalC6.style.visibility = "visible";
+  modal6.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function () {
+  modal6.classList.toggle("modal-close");
+
+  setTimeout(function () {
+    modalC6.style.opacity = "0";
+    modalC6.style.visibility = "hidden";
+  }, 600);
+});
+
+window.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target == modalC6) {
+    modal6.classList.toggle("modal-close");
+
+    setTimeout(function () {
+      modalC6.style.opacity = "0";
+      modalC6.style.visibility = "hidden";
+    }, 900);
+  }
+});
+
+//=============== menu hamburguesa ===============
+
+const hamburguer = document.querySelector(".hamburguer");
+const menu = document.querySelector(".menu-navegacion");
+
+hamburguer.addEventListener("click", () => {
+  menu.classList.toggle("spread");
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("spread") &&
+    e.target != menu &&
+    e.target != hamburguer
+  ) {
+    console.log("cerrar");
+    menu.classList.toggle("spread");
+  }
+});
