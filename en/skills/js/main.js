1d3e9ddffeff479f57d0664d457d43ed7fae5ae3
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-30 11:09:26 -0500

    45435

--- en/skills/js/main.js
+++ en/skills/js/main.js
@@ -1,32 +1,25 @@
-let cerrar = document.querySelectorAll(".close")[0];
-let abrir = document.querySelectorAll(".cta")[0];
-let modal = document.querySelectorAll(".modal")[0];
-let modalC = document.querySelectorAll(".modal-container")[0];
 
-abrir.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalC.style.opacity = "1";
-  modalC.style.visibility = "visible";
-  modal.classList.toggle("modal-close");
+
+
+const hamburguer = document.querySelector('.hamburguer')
+const menu = document.querySelector('.menu-navegacion')
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(menu.classList.contains('spread') 
+        && e.target != menu && e.target != hamburguer){
+        console.log('cerrar')
+        menu.classList.toggle("spread")
+    }
+})
+
+
+
+document.getElementById("html").addEventListener("mouseover", function(){
+  document.getElementById("p-html").style.display = "block";
 });
 
-cerrar.addEventListener("click", function () {
-  modal.classList.toggle("modal-close");
 
-  setTimeout(function () {
-    modalC.style.opacity = "0";
-    modalC.style.visibility = "hidden";
-  }, 600);
-});
-
-window.addEventListener("click", function (e) {
-  console.log(e.target);
-  if (e.target == modalC) {
-    modal.classList.toggle("modal-close");
-
-    setTimeout(function () {
-      modalC.style.opacity = "0";
-      modalC.style.visibility = "hidden";
-    }, 900);
-  }
-});
