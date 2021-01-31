f243f5ea2e94d7a86e90ae021b62ad7f1c7e5eb8
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-01-30 22:19:22 -0500

    35

--- es/contacto/enviar.php
+++ es/contacto/enviar.php
@@ -1,5 +1,5 @@
 <?php
-$name = $_POST['name'];
+$name = $_POST['nombre'];
 $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
@@ -9,7 +9,7 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message = "Mensaje enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
